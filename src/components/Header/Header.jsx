a9d7e831b076db2b13a06841d2f32041e9ff3020
Ugwuanyi Love Ekene <49119675+Hubnotch@users.noreply.github.com>
2022-12-19 05:02:00 +0100

    fixed the navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -19,7 +19,7 @@ function Header() {
           <img src={logo} alt="sytycc logo" />
         </Link>
       </div>   
-      <div className="hidden md:w-3/4 md:flex md:items-center md:justify-between">
+      <div className="hidden md:w-3/4  md:items-center md:justify-between">
         <Link onClick={closeMenu} to='/about'>About Us</Link>
         <Link onClick={closeMenu} to='/profiles'>Profiles</Link>
         <Link onClick={closeMenu} to='/stories'>Stories</Link>
@@ -28,7 +28,7 @@ function Header() {
         <Link to='/'><button className='text-white bg-[#06113C] px-9 py-3 rounded-xl flex items-center'>Login</button></Link>
       </div>   
       </div>
-      <div className={isOpen ? 'ease-in-out duration-700 h-auto text-[#F5F5F5] bg-[#06113C] absolute top-[100px] left-0 w-screen flex items-center flex-col justify-between':'hidden'}>
+      <div className={isOpen ? 'ease-in-out duration-700 h-auto text-[#F5F5F5] bg-[#06113C] absolute top-[100px] left-0 w-screen hidden items-center flex-col justify-between':'hidden'}>
         <Link className='my-3 ' onClick={closeMenu} to='/about'>About Us</Link>
         <Link className='my-3 ' onClick={closeMenu} to='/profiles'>Profiles</Link>
         <Link className='my-3 ' onClick={closeMenu} to='/stories'>Stories</Link>
@@ -37,7 +37,7 @@ function Header() {
         <Link to='/'><button  onClick={closeMenu} className='text-white bg-[navy] border border-sky-400 px-9 py-3 rounded-xl flex items-center'>Login</button></Link>
       </div>   
       </div>
-      <div className="md:hidden" onClick={handleChange}>
+      <div className="hidden md:hidden" onClick={handleChange}>
  {isOpen ? <AiOutlineClose className='text-3xl cursor-pointer' /> : <BiMenuAltRight className='text-3xl cursor-pointer ease-in-out duration-1000' />}
       </div>
       </nav>
